6dd6e5b205d1fdbdf7dce885902020e9a3c252ac
Jurie_OV <jurie.spies@openvantage.co.za>
2021-06-06 18:50:14 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [userArea, setUserArea] = useState('');
   const [userProvince, setUserProvince] = useState('');
+  const [loadsheddingStatus, setLoadsheddingStatus] = useState('');
+  const statusUrl = 'https://ewn.co.za/assets/loadshedding/api/eskomstatus';
+  const areaUrl = 'https://loadshedding.eskom.co.za/LoadShedding/FindSuburbs?searchText=monument park&maxResults=3';
+
+  const axiosFunc = async (url, header, params) => {
+    const config = {
+      method: 'get',
+      url,
+      header,
+      params,
+    };
+    return axios(config).catch((res) => { console.log('❌❌❌', res); });
+  };
+
+  useEffect(() => {
+    axiosFunc(statusUrl).then((res) => { setLoadsheddingStatus(res.data); });
+    // axiosFunc(areaUrl).then((res) => { console.log('🚀 example', res); });
+  }, []);
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <span>{loadsheddingStatus}</span>
       <input placeholder="Type in your suburb/village/area" onChange={(val) => setUserArea(val.target.value)} />
       <select onChange={(val) => setUserProvince(val.target.value)}>
         <option disabled selected value="">select an option</option>
